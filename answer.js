bafc9febfea5a6e33c48b5da752eaeff480c28e4
Shawn Otomo <kodama78@gmail.com>
2015-05-24 21:00:41 -0700

    number 5 done

--- answer.js
+++ answer.js
@@ -1,8 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<script type="text/javascript">
+<script type="text/javascript">
 	/* DAILY CHALLENGE 001 */
 	var haystack = ['cat','dog','bird','turtle','lizard']; 
 	var needle = ['dog','lizard','flower','monkey','unicorn']; 
@@ -36,7 +32,7 @@
 		return output;
 	}
 
-	/* DILY CHALLENGE 003 */
+	/* DAILY CHALLENGE 003*/
 	var array1 = [ [1,2,3], [3,4,5], [6,7,8] ]; 
 	var array2 = [ [1,2,3], [1,2,3], [1,2,3] ];
 	var output = null;
@@ -59,9 +55,33 @@
 		}
 		return output;
 	}
-	</script>
-</head>
-<body>
+	/* DAILY CHALLENGE 004*/
+	function find_factors(factor_number){
+		var output = [];
+		for (var i = 1; i <= factor_number; i++){
+			if (factor_number % i == 0){
+				output.push(i);
+			}
+		}
+		return output;
+	}
 
-</body>
-</html>
+	/* DAILY CHALLENGE 005 */
+	var input_array = 
+	[
+    {name: 'John', age:42, gender:'M'},
+    {name: 'Jack', age:34, gender:'M'},
+    {name: 'Michael',age:40, gender: 'M'},
+    {name: 'Kendra',age:48, gender:'F'}
+	]
+		function sort_object_list_by_name(list_array){
+			for (var i = 0; i < list_array.length-1; i++){
+				if(list_array[i].name > list_array[i+1].name){
+					var temp = list_array[i];
+					list_array[i] = list_array[i+1];
+					list_array[i+1] = temp;
+				}
+			}
+			return list_array;
+		}
+	</script>
